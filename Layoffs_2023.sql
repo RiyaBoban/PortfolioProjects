7d492c097774f650fcc077c42774b36be351b201
RiyaBoban <129033360+RiyaBoban@users.noreply.github.com>
2023-05-01 21:36:47 +0530

    Update Layoffs_2023.sql

--- Layoffs_2023.sql
+++ Layoffs_2023.sql
@@ -1,6 +1,21 @@
 
 SELECT *
+  FROM [Portfolio Project].[dbo].[layoffs_2023];
+  
+---- Finding Duplicates
+
+  SELECT *,count(*)
   FROM [Portfolio Project].[dbo].[layoffs_2023]
+  GROUP BY company,location, industry, total_laid_off,percentage_laid_off,date,stage, country,funds_raised
+  HAVING Count(*)>1;
+
+---- Delete Duplicates
+
+WITH cte AS(
+SELECT* ,ROW_NUMBER() OVER (PARTITION BY company,location, industry, total_laid_off,percentage_laid_off,date,stage, country,funds_raised ORDER BY company) AS Duplicate_count
+FROM [Portfolio Project].[dbo].[layoffs_2023])
+DELETE FROM cte
+WHERE Duplicate_count>1;  
 
 ---- Delete rows with null values in total_laid_off and percentage_laid_off
 
@@ -31,7 +46,7 @@ WHERE total_laid_off !=0
 GROUP BY country)
 SELECT country,ROUND(100.0*sum_of_layoffs/(SELECT SUM(CAST( total_laid_off AS FLOAT)) FROM  [Portfolio Project].[dbo].[layoffs_2023]),2) AS Percentage_of_layoffs
 FROM cte
-ORDER BY Percentage_of_layoffs DESC
+ORDER BY Percentage_of_layoffs DESC;
 
 -----Which industries are highly risky? 
 
@@ -45,7 +60,7 @@ FROM CTE)
 SELECT Industry,Count,
 CASE WHEN count> median THEN 'High_Layoffs'
      WHEN count< median THEN 'Low_Layoffs' END AS Status
-FROM cte_median
+FROM cte_median;
 
 -----Company which had maximum layoffs in each country
 
@@ -57,26 +72,26 @@ GROUP BY company,country
 SELECT company,layoffs_number, country
 FROM cte
 WHERE rank =1 AND layoffs_number !=0
-ORDER BY layoffs_number DESC
+ORDER BY layoffs_number DESC;
 
 -----Update blank date
 
 UPDATE [Portfolio Project].[dbo].[layoffs_2023]
 SET  date = '2023-02-23' 
-WHERE company = 'blackbaud'
+WHERE company = 'blackbaud';
 
 -----Which month had maximum layoffs
 
 SELECT SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number,DATEPART(Month,date) as month
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY DATEPART(month,date)
-ORDER BY layoffs_number DESC
+ORDER BY layoffs_number DESC;
 
 -----Number of layoffs yearwise
 SELECT SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number,DATEPART(YEAR,date) as year
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY DATEPART(YEAR,date)
-ORDER BY year
+ORDER BY year;
 
 -----Top 3 Locations where most layoffs happened year-wise
 
@@ -87,14 +102,14 @@ FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY location,DATEPART(YEAR,date)
 ) AS location
 WHERE rank <=3
-ORDER BY rank,year
+ORDER BY rank,year;
 
 -----Top 10 Industries with maximum  layoffs
 
 SELECT TOP 10 Industry, SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY industry
-ORDER BY layoffs_number DESC
+ORDER BY layoffs_number DESC;
 
 -----Companies which raised maximum fund and with Maximum Percentage of Layoffs
 
@@ -111,7 +126,7 @@ SELECT cf.company, cf.Maximum_fund_raised, cp.maximum_percentage
 FROM cte_fund AS cf
 INNER JOIN cte_percent AS cp
 ON cf.company = cp.company
-ORDER BY Maximum_fund_raised DESC
+ORDER BY Maximum_fund_raised DESC;
 
 -----At what stage maximum number of employees are laid off 
 
@@ -119,7 +134,7 @@ SELECT stage, SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, count(*) AS 
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE stage!=' '
 GROUP BY stage
-ORDER BY Count DESC
+ORDER BY Count DESC;
 
 -----Layoffs in each city of USA
 
@@ -127,7 +142,7 @@ SELECT location,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'United States'
 GROUP BY location
-ORDER BY layoffs_number DESC
+ORDER BY layoffs_number DESC;
 
 -----Companies in India that laid off employees
 
@@ -135,7 +150,7 @@ SELECT company,location,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'India' AND total_laid_off !=0
 GROUP BY location,company
-ORDER BY layoffs_number DESC
+ORDER BY layoffs_number DESC;
 
 -----Number of companies laid off in India locationwise
 
@@ -143,20 +158,20 @@ SELECT location,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, count(*) A
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'India' AND total_laid_off !=0
 GROUP BY location
-ORDER BY Number_of_Companies DESC
+ORDER BY Number_of_Companies DESC;
 
 -----Number of companies laid off  in each countries
 
 SELECT Country,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, count(*) AS Number_of_Companies
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY country
-ORDER BY Number_of_Companies DESC
+ORDER BY Number_of_Companies DESC;
 
 -----Rolling/Moving Count of Lay offs in India
 
 SELECT company,location,SUM(CAST( total_laid_off AS FLOAT)) OVER(PARTITION BY location ORDER BY total_laid_off) AS Running_Total
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'India' AND total_laid_off !=0
-ORDER BY location
+ORDER BY location;
 
 
