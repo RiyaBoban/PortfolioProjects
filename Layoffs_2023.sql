bcdd6a1af15cef86030b84696ea06f8d6fbbfe19
RiyaBoban <129033360+RiyaBoban@users.noreply.github.com>
2023-05-02 12:04:03 +0530

    Update Layoffs_2023.sql

--- Layoffs_2023.sql
+++ Layoffs_2023.sql
@@ -22,17 +22,21 @@ WHERE Duplicate_count>1;
 DELETE FROM layoffs_2023 
 WHERE total_laid_off = ' ' AND percentage_laid_off = ' ';
 
+----- Change Datatype of total_laid_off column
+
+ALTER TABLE [Portfolio Project].[dbo].[layoffs_2023] 
+ALTER COLUMN [total_laid_off] FLOAT
 
 ----- Top 5 Companies with highest number of layoffs 
 
-SELECT  TOP 5 company, SUM(CAST(total_laid_off AS FLOAT)) AS maximum_layoff
+SELECT  TOP 5 company, SUM(total_laid_off ) AS maximum_layoff
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY company
 ORDER BY maximum_layoff DESC;
 
 ----Top 5 Countries which has maximum layoffs
 
-SELECT TOP 5 SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, country
+SELECT TOP 5 SUM(total_laid_off ) AS layoffs_number, country
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY country
 ORDER BY layoffs_number DESC;
@@ -40,17 +44,19 @@ ORDER BY layoffs_number DESC;
 -----Percentage of Layoff per country over total number layoffs
 
 WITH cte AS(
-SELECT country, SUM(CAST( total_laid_off AS FLOAT))AS sum_of_layoffs
+SELECT country, SUM( total_laid_off )AS sum_of_layoffs
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE total_laid_off !=0
 GROUP BY country)
-SELECT country,ROUND(100.0*sum_of_layoffs/(SELECT SUM(CAST( total_laid_off AS FLOAT)) FROM  [Portfolio Project].[dbo].[layoffs_2023]),2) AS Percentage_of_layoffs
+SELECT country,ROUND(100.0*sum_of_layoffs/(SELECT SUM( total_laid_off) 
+FROM  [Portfolio Project].[dbo].[layoffs_2023]),2) AS Percentage_of_layoffs
 FROM cte
 ORDER BY Percentage_of_layoffs DESC;
 
 -----Which industries are highly risky? 
 
-WITH cte AS(SELECT DISTINCT INDUSTRY, COUNT(industry) OVER(PARTITION BY industry) AS Count
+WITH cte AS(
+SELECT DISTINCT INDUSTRY, COUNT(industry) OVER(PARTITION BY industry) AS Count
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 ),
 cte_median AS (SELECT  INDUSTRY, Count,
@@ -65,7 +71,7 @@ FROM cte_median;
 -----Company which had maximum layoffs in each country
 
 WITH cte AS(
-SELECT company, SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number,country, ROW_NUMBER()OVER(PARTITION BY country ORDER BY SUM(CAST(total_laid_off AS FLOAT)) DESC) AS rank
+SELECT company, SUM(total_laid_off) AS layoffs_number,country, ROW_NUMBER()OVER(PARTITION BY country ORDER BY SUM(total_laid_off) DESC) AS rank
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY company,country
 )
@@ -88,7 +94,7 @@ GROUP BY DATEPART(month,date)
 ORDER BY layoffs_number DESC;
 
 -----Number of layoffs yearwise
-SELECT SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number,DATEPART(YEAR,date) as year
+SELECT SUM(total_laid_off ) AS layoffs_number,DATEPART(YEAR,date) as year
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY DATEPART(YEAR,date)
 ORDER BY year;
@@ -96,8 +102,8 @@ ORDER BY year;
 -----Top 3 Locations where most layoffs happened year-wise
 
 SELECT location, layoffs_number, year,rank
-FROM (SELECT location, SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, DATEPART(YEAR,date) as year, 
-ROW_NUMBER() OVER(PARTITION BY DATEPART(YEAR,date)  ORDER BY  SUM(CAST(total_laid_off AS FLOAT)) DESC ) AS rank
+FROM (SELECT location, SUM(total_laid_off) AS layoffs_number, DATEPART(YEAR,date) as year, 
+ROW_NUMBER() OVER(PARTITION BY DATEPART(YEAR,date)  ORDER BY  SUM(total_laid_off ) DESC ) AS rank
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY location,DATEPART(YEAR,date)
 ) AS location
@@ -106,7 +112,7 @@ ORDER BY rank,year;
 
 -----Top 10 Industries with maximum  layoffs
 
-SELECT TOP 10 Industry, SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number
+SELECT TOP 10 Industry, SUM(total_laid_off ) AS layoffs_number
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY industry
 ORDER BY layoffs_number DESC;
@@ -130,7 +136,7 @@ ORDER BY Maximum_fund_raised DESC;
 
 -----At what stage maximum number of employees are laid off 
 
-SELECT stage, SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, count(*) AS Count
+SELECT stage, SUM(total_laid_off ) AS layoffs_number, count(*) AS Count
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE stage!=' '
 GROUP BY stage
@@ -138,7 +144,7 @@ ORDER BY Count DESC;
 
 -----Layoffs in each city of USA
 
-SELECT location,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number
+SELECT location,SUM(total_laid_off) AS layoffs_number
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'United States'
 GROUP BY location
@@ -146,7 +152,7 @@ ORDER BY layoffs_number DESC;
 
 -----Companies in India that laid off employees
 
-SELECT company,location,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number
+SELECT company,location,SUM(total_laid_off)) AS layoffs_number
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'India' AND total_laid_off !=0
 GROUP BY location,company
@@ -154,7 +160,7 @@ ORDER BY layoffs_number DESC;
 
 -----Number of companies laid off in India locationwise
 
-SELECT location,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, count(*) AS Number_of_Companies
+SELECT location,SUM(total_laid_off ) AS layoffs_number, count(*) AS Number_of_Companies
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'India' AND total_laid_off !=0
 GROUP BY location
@@ -162,14 +168,14 @@ ORDER BY Number_of_Companies DESC;
 
 -----Number of companies laid off  in each countries
 
-SELECT Country,SUM(CAST(total_laid_off AS FLOAT)) AS layoffs_number, count(*) AS Number_of_Companies
+SELECT Country,SUM(total_laid_off ) AS layoffs_number, count(*) AS Number_of_Companies
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY country
 ORDER BY Number_of_Companies DESC;
 
 -----Rolling/Moving Count of Lay offs in India
 
-SELECT company,location,SUM(CAST( total_laid_off AS FLOAT)) OVER(PARTITION BY location ORDER BY total_laid_off) AS Running_Total
+SELECT company,location,SUM( total_laid_off ) OVER(PARTITION BY location ORDER BY total_laid_off) AS Running_Total
 FROM [Portfolio Project].[dbo].[layoffs_2023]
 WHERE country = 'India' AND total_laid_off !=0
 ORDER BY location;
