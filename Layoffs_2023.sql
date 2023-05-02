ba0bf4a2316a8f670e1a298dc6bb49b7ab090fa8
RiyaBoban <129033360+RiyaBoban@users.noreply.github.com>
2023-05-02 12:46:42 +0530

    Layoffs_2023.sql

--- Layoffs_2023.sql
+++ Layoffs_2023.sql
@@ -1,32 +1,4 @@
 
-SELECT *
-  FROM [Portfolio Project].[dbo].[layoffs_2023];
-  
----- Finding Duplicates
-
-  SELECT *,count(*)
-  FROM [Portfolio Project].[dbo].[layoffs_2023]
-  GROUP BY company,location, industry, total_laid_off,percentage_laid_off,date,stage, country,funds_raised
-  HAVING Count(*)>1;
-
----- Delete Duplicates
-
-WITH cte AS(
-SELECT* ,ROW_NUMBER() OVER (PARTITION BY company,location, industry, total_laid_off,percentage_laid_off,date,stage, country,funds_raised ORDER BY company) AS Duplicate_count
-FROM [Portfolio Project].[dbo].[layoffs_2023])
-DELETE FROM cte
-WHERE Duplicate_count>1;  
-
----- Delete rows with null values in total_laid_off and percentage_laid_off
-
-DELETE FROM layoffs_2023 
-WHERE total_laid_off = ' ' AND percentage_laid_off = ' ';
-
------ Change Datatype of total_laid_off column
-
-ALTER TABLE [Portfolio Project].[dbo].[layoffs_2023] 
-ALTER COLUMN [total_laid_off] FLOAT
-
 ----- Top 5 Companies with highest number of layoffs 
 
 SELECT  TOP 5 company, SUM(total_laid_off ) AS maximum_layoff
@@ -41,7 +13,7 @@ FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY country
 ORDER BY layoffs_number DESC;
 
------Percentage of Layoff per country over total number layoffs
+-----Percentage of Layoff per country over total number of layoffs
 
 WITH cte AS(
 SELECT country, SUM( total_laid_off )AS sum_of_layoffs
@@ -68,7 +40,16 @@ CASE WHEN count> median THEN 'High_Layoffs'
      WHEN count< median THEN 'Low_Layoffs' END AS Status
 FROM cte_median;
 
------Company which had maximum layoffs in each country
+----- In each country which industry has maximum layoffs?
+
+SELECT country,industry,layoffs_number 
+FROM (SELECT country,industry,sum(total_laid_off) AS layoffs_number,ROW_NUMBER() OVER(PARTITION BY country ORDER BY sum(total_laid_off)DESC) AS Rank
+  FROM [Portfolio Project].[dbo].[layoffs_2023]
+  GROUP BY country,industry)AS country_layoffs
+  WHERE Rank =1 AND layoffs_number !=0
+  ORDER BY layoffs_number DESC
+  
+  -----Company which had maximum layoffs in each country
 
 WITH cte AS(
 SELECT company, SUM(total_laid_off) AS layoffs_number,country, ROW_NUMBER()OVER(PARTITION BY country ORDER BY SUM(total_laid_off) DESC) AS rank
@@ -80,11 +61,12 @@ FROM cte
 WHERE rank =1 AND layoffs_number !=0
 ORDER BY layoffs_number DESC;
 
------Update blank date
+-----Rolling/Moving Count of Lay offs in India
 
-UPDATE [Portfolio Project].[dbo].[layoffs_2023]
-SET  date = '2023-02-23' 
-WHERE company = 'blackbaud';
+SELECT company,location,SUM( total_laid_off ) OVER(PARTITION BY location ORDER BY total_laid_off) AS Running_Total
+FROM [Portfolio Project].[dbo].[layoffs_2023]
+WHERE country = 'India' AND total_laid_off !=0
+ORDER BY location;
 
 -----Which month had maximum layoffs
 
@@ -110,14 +92,7 @@ GROUP BY location,DATEPART(YEAR,date)
 WHERE rank <=3
 ORDER BY rank,year;
 
------Top 10 Industries with maximum  layoffs
-
-SELECT TOP 10 Industry, SUM(total_laid_off ) AS layoffs_number
-FROM [Portfolio Project].[dbo].[layoffs_2023]
-GROUP BY industry
-ORDER BY layoffs_number DESC;
-
------Companies which raised maximum fund and with Maximum Percentage of Layoffs
+-----Companies which raised maximum fund and their Maximum Percentage of Layoffs
 
 WITH cte_fund AS(
 SELECT company,SUM(CAST(funds_raised AS FLOAT)) as Maximum_fund_raised
@@ -173,11 +148,6 @@ FROM [Portfolio Project].[dbo].[layoffs_2023]
 GROUP BY country
 ORDER BY Number_of_Companies DESC;
 
------Rolling/Moving Count of Lay offs in India
 
-SELECT company,location,SUM( total_laid_off ) OVER(PARTITION BY location ORDER BY total_laid_off) AS Running_Total
-FROM [Portfolio Project].[dbo].[layoffs_2023]
-WHERE country = 'India' AND total_laid_off !=0
-ORDER BY location;
 
 
