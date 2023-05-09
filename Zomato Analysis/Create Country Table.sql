9104cb8046c8a26c3c479f662719e9dcbe550d38
RiyaBoban <129033360+RiyaBoban@users.noreply.github.com>
2023-05-09 10:37:29 +0530

    Add files via upload

---
+++ Zomato Analysis/Create Country Table.sql
@@ -0,0 +1,7 @@
+----- Create a new table which includes the country name and country code
+
+CREATE TABLE Country(countrycode int, country varchar(50));
+
+INSERT INTO Country (countrycode,country)
+VALUES(1,'India'),(14,'Australia'),(148,'NewZealand'),(162,'Philippines'),(166,'Qatar'),(184,'Singapore'),(189,'SouthAfrica'),(191,'SriLanka'),
+(208,'Turkey'),(214,'UAE'),(215,'UK'),(216,'USA'),(30,'Brazil'),(37,'Canada'),(94,'Indonesia')
